b8a502af18367cef099d35c7324a0c1a02547b51
Steven Ernits <sterni@taltech.ee>
2024-04-29 11:18:59 +0300

    188/0/7 Fixed and implemented Facade tests

---
+++ Tests/Facade/Project/PartSpecificationViewModelTests.cs
@@ -0,0 +1,16 @@
+﻿using HaSe.Facade;
+using HaSe.Facade.Project;
+using HaSe.Tests.Helpers;
+
+namespace HaSe.Tests.Facade.Project
+{
+    [TestClass]
+    public class PartSpecificationViewModelTests : SealedNewTests<PartSpecificationViewModel, EntityViewModel>
+    {
+        [TestMethod] public void DescriptionTest() => PropertyTest("Description", true);
+        [TestMethod] public void TypeTest() => PropertyTest("Type");
+        [TestMethod] public void DateDocumentedTest() => PropertyTest("Date documented");
+        [TestMethod] public void CommentTest() => PropertyTest("Comment");
+        [TestMethod] public void PartIdTest() => PropertyTest("Part Id");
+    }
+}
---
+++ Tests/Facade/Project/PartViewModelTests.cs
@@ -0,0 +1,14 @@
+﻿using HaSe.Facade;
+using HaSe.Facade.Project;
+using HaSe.Tests.Helpers;
+
+namespace HaSe.Tests.Facade.Project
+{
+    [TestClass]
+    public class PartViewModelTests : SealedNewTests<PartViewModel, EntityViewModel>
+    {
+        [TestMethod] public void NameTest() => PropertyTest("Name", true);
+        [TestMethod] public void TypeTest() => PropertyTest("Type", true);
+
+    }
+}
