4308cbc9dc6ebb6577e449caeace391ff064ca3c
Steven Ernits <sterni@taltech.ee>
2024-05-02 11:22:11 +0300

    131/1/9 Added PartSpecificationStatusController

---
+++ Soft/Controllers/PartSpecificationStatusController.cs
@@ -0,0 +1,23 @@
+﻿using HaSe.Data.Project;
+using HaSe.Domain.Project;
+using HaSe.Domain.Repos;
+using HaSe.Facade.Project;
+using HaSe.Helpers.Methods;
+using HaSe.Infra.Project;
+
+namespace HaSe.Soft.Controllers {
+    public class PartSpecificationStatusController(IPartSpecificationStatusRepo repo, IPartSpecificationsRepo? partsRepo = null) : BaseController<PartSpecificationStatus, PartSpecificationStatusViewModel>(repo) {
+
+        protected override string selectItemTextField => nameof(PartSpecificationStatusViewModel.FromDate);
+        protected override PartSpecificationStatus ToModel(PartSpecificationStatusViewModel viewmodel) {
+            return new PartSpecificationStatus(PropertyCopier.CopyPropertiesFrom<PartSpecificationStatusViewModel, PartSpecificationStatusData>(viewmodel));
+        }
+
+        protected override async Task PopulateRelatedItems(PartSpecificationStatus? model) {
+            await base.PopulateRelatedItems(model);
+            if(partsRepo is null)
+                return;
+            ViewBag.Parts = await new PartSpecificationController(partsRepo).SelectListAsync();
+        }
+    }
+}
