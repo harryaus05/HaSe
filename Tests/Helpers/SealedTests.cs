cebf39ed20da41f08a5f775ce7b261491b5f92a9
Steven Ernits <sterni@taltech.ee>
2024-05-13 18:11:55 +0300

    141/0/8 Implemented tests for PartSpecificationStatus

--- Tests/Helpers/SealedTests.cs
+++ Tests/Helpers/SealedTests.cs
@@ -1,7 +1,11 @@
-﻿namespace HaSe.Tests.Helpers;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public abstract class SealedTests<TClass, TBaseClass> : ClassTests<TClass, TBaseClass> where TClass : class {
-    [TestMethod] public void IsSealedTest() {
-        Assert.IsTrue(typeof(TClass)?.IsSealed);
+namespace HaSe.Tests.Helpers {
+    public abstract class SealedTests<TClass, TBaseClass> : ClassTests<TClass, TBaseClass> where TClass : class {
+        [TestMethod] public void IsSealedTest() => Assert.IsTrue(typeof(TClass)?.IsSealed);
     }
 }
