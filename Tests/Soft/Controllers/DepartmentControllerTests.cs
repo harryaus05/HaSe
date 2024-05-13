42cc27d50e857e26e94f8e8fefcc725f983a1d5a
Steven Ernits <sterni@taltech.ee>
2024-05-13 18:56:42 +0300

    187/0/5 Removed unnecessary class HtmlEditEnum

--- Tests/Soft/Controllers/DepartmentControllerTests.cs
+++ Tests/Soft/Controllers/DepartmentControllerTests.cs
@@ -1,26 +1,3 @@
 ﻿namespace HaSe.Tests.Soft.Controllers {
-    //[TestClass]
-    //public class DepartmentControllerTests {
-    //    private Mock<IPartSpecificationsRepo> _mockRepo;
-    //    private PartSpecificationController _controller;
 
-    //    [TestInitialize]
-    //    public void TestInitialize() {
-    //        _mockRepo = new Mock<IPartSpecificationsRepo>();
-    //        _controller = new PartSpecificationController(_mockRepo.Object);
-    //    }
-
-    //    [TestMethod]
-    //    public async Task TestIndex() {
-    //        var sortOrder = "name";
-    //        var searchString = "test";
-    //        var pageNumber = 1;
-
-    //        var result = await _controller.Index(sortOrder, searchString, pageNumber) as ViewResult;
-
-    //        Assert.IsNotNull(result);
-    //        var model = result.Model as List<PartSpecificationViewModel>;
-    //        Assert.IsNotNull(model); 
-    //    }
-    //}
 }
