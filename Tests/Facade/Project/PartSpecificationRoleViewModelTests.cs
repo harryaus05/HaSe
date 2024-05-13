d0906ed147ec47c8c0e3884b20e43e563bd2b8d5
Steven Ernits <sterni@taltech.ee>
2024-05-13 18:49:08 +0300

    187/0/5 Implemented tests for PartSpecificationStatusViewModel and PartSpecificationRoleViewModel. IsFacadeTested now passes

---
+++ Tests/Facade/Project/PartSpecificationRoleViewModelTests.cs
@@ -0,0 +1,16 @@
+﻿using HaSe.Facade;
+using HaSe.Facade.Project;
+using HaSe.Tests.Helpers;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace HaSe.Tests.Facade.Project
+{
+    [TestClass]
+    public class PartSpecificationRoleViewModelTests : SealedNewTests<PartSpecificationRoleViewModel, EntityViewModel>
+    {
+        [TestMethod] public void PartyNameTest() => PropertyTest("Party Name", true);
+        [TestMethod] public void TypeTest() => PropertyTest("Type", true);
+       
+    }
+}
