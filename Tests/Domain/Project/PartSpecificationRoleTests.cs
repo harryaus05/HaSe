4d7ebbea0525a9f93abd927e03634039a1b1d0d3
Steven Ernits <sterni@taltech.ee>
2024-05-13 18:20:06 +0300

    151/0/8 Implemented tests for PartSpecificationRole

---
+++ Tests/Domain/Project/PartSpecificationRoleTests.cs
@@ -0,0 +1,12 @@
+﻿using HaSe.Data.Project;
+using HaSe.Domain.Project;
+
+namespace HaSe.Tests.Domain.Project
+{
+    [TestClass]
+    public class PartSpecificationRoleTests : DomainClassTests<PartSpecificationRole, PartSpecificationRoleData>
+    {
+        [TestMethod] public void PartyNameTest() => ValueTest();
+        [TestMethod] public void TypeTest() => ValueTest();
+    }
+}
