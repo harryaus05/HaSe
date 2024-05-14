583d3d44bae332b34111b104289ab9d6cdce36da
Steven Ernits <sterni@taltech.ee>
2024-05-15 00:02:14 +0300

    select2 implemented, dropdowns now say "Results could not be loaded"

--- Data/Project/PartSpecificationStatusData.cs
+++ Data/Project/PartSpecificationStatusData.cs
@@ -3,10 +3,5 @@
         public DateTime FromDate { get; set; }
         public DateTime ThruDate { get; set; }
         public string Type { get; set; }
-        public int PartId { get; set; }
-    }
-
-    public abstract class PartSpecificationRelationData : EntityData {
-        public int PartSpecificationId { get; set; }
     }
 }
---
+++ Soft/wwwroot/js/select2.js
@@ -0,0 +1,41 @@
+﻿$(".selectItems2").each(function () {
+
+    var $this = $(this),
+        selectedId = $this.data('id'),
+        controller = $this.data('controller');
+
+    if (selectedId) {
+        $.getJSON(`/${controller}/selectItem`, { id: selectedId }, function (data) {
+            $this.append(new Option(data.text, data.value, true, true)).trigger('change');
+        });
+    }
+
+    $this.select2({
+
+        placeholder: `Select a value`,
+        theme: "bootstrap4",
+        allowClear: true,
+
+        ajax: {
+            url: `/${controller}/selectItems`,
+            data: function (params) {
+                return {
+                    searchString: params.term,
+                    id: selectedId
+                };
+            },
+            processResults: function (result) {
+                return {
+                    results: $.map(result, function (item) {
+                        return {
+                            id: item.value,
+                            text: item.text,
+                            selected: item.selected
+                        };
+                    }),
+                };
+            }
+        }
+    });
+
+});
