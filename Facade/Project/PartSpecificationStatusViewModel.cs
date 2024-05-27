e5b99712cca48a1698334ef48f4a5891185d8e7c
Steven Ernits <sterni@taltech.ee>
2024-05-27 15:40:42 +0300

    179/0/14 - Fixed Thru Date not showing in tables, it is no longer nullable

--- Facade/Project/PartSpecificationStatusViewModel.cs
+++ Facade/Project/PartSpecificationStatusViewModel.cs
@@ -5,7 +5,7 @@ using System.ComponentModel;
 namespace HaSe.Facade.Project {
     [Description("Part Specification Status")] public sealed class PartSpecificationStatusViewModel : EntityViewModel {
         [DataType(DataType.Date)] [Required, DisplayName("From Date"), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)] public DateTime FromDate { get; set; }
-        [DataType(DataType.Date)] [DisplayName("Thru Date"), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)] public DateTime? ThruDate { get; set; }
+        [DataType(DataType.Date)] [DisplayName("Thru Date"), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)] public DateTime ThruDate { get; set; }
         [DisplayName("Type")] [Required] public string Type { get; set; }
         [DisplayName("PartSpecification")] public int PartSpecificationId { get; set; }
     }
