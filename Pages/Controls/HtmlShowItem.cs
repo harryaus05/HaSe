78934edffba1fce26f68913105b202a63afdf954
Harry Aus <ausharry5@gmail.com>
2024-05-31 02:41:33 +0300

    Misc items

--- Pages/Controls/HtmlShowItem.cs
+++ Pages/Controls/HtmlShowItem.cs
@@ -4,10 +4,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HaSe.Pages.Controls;
 
+
 public static class HtmlShowItem {
     public static IHtmlContent ShowItem<TModel, TValue>(this IHtmlHelper<TModel> h, Expression<Func<TModel, TValue>> e, string? value = null) {
         var lab = h.LabelFor(e);
-        var val = (value is null)? h.DisplayFor(e): h.Raw(value);
+        var val = (value is null) ? h.DisplayFor(e) : h.Raw(value);
 
         return HtmlControl.Control(lab, val);
     }
